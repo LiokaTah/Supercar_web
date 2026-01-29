1123bf54a97b4770885de817f1546b9a6aa00c81
LiokaTah <tahinalioka282@gmail.com>
2026-01-29 10:29:12 +0400

    Update page_voiture.php

--- pages/page_voiture.php
+++ pages/page_voiture.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projet Supercar</title>
-    <link rel="stylesheet" href="style33.css"> 
+    <link rel="stylesheet" href="../assets/css/style33.css"> 
 </head>
 <body>
 
